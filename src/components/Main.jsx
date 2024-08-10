d985def9aebb0a52f20a79938746565073373731
Vishwajeet Vishwakarma <144940737+V-Vishwakarma@users.noreply.github.com>
2024-08-10 13:30:51 +0530

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Main(props) {
+    const { data } = props
+
+    return (
+
+        <div className='imgContainer'>
+
+            <img src={data.url} alt={data.title} className='bgImage' />
+
+        </div>
+    )
+}
